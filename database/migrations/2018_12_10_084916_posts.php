0407a7ce77c49e1c210475e8df72af5376d68bfa
laykith <laykith@gmail.com>
2018-12-10 10:10:25 +0100

    Created post migration

---
+++ database/migrations/2018_12_10_084916_posts.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Posts extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('title', 64);
+            //Add image field
+            $table->text('content');
+            $table->timestamp('created_at');
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
