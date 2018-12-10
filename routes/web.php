9ea9ad049ad8df25be7835dc17fe1731c8582d66
laykith <laykith@gmail.com>
2018-12-10 10:17:08 +0100

    Added Laravel login scaffolding

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/account', function () {
+    return view('account');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
