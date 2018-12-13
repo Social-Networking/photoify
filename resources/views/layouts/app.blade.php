3914393528a63b58284b9d4229be09da45c9e739
laykith <laykith@gmail.com>
2018-12-13 10:35:48 +0100

    Updated links in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,19 +32,15 @@
                     <a class="navbar-item" href="{{ url('/') }}">
                         Home
                     </a>
-
-                    <a class="navbar-item" href="{{ url('/account') }}">
-                        Account
-                    </a>
                 </div>
 
                 <div class="navbar-end">
                     <div class="navbar-item">
                         <div class="buttons">
-                            <a class="button is-primary">
+                            <a class="button is-primary" href="{{ route('register') }}">
                                 <strong>Sign up</strong>
                             </a>
-                            <a class="button is-light">
+                            <a class="button is-light" href="{{ route('login') }}">
                                 Log in
                             </a>
                         </div>
