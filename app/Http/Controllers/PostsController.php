a87e980319273b9f67394469247c27a58236a395
laykith <laykith@gmail.com>
2018-12-23 16:29:55 +0100

    Added limited fetch on posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -24,7 +24,7 @@ class PostsController extends Controller
     public function index()
     {
         return view('posts.index', [
-            'posts' => Post::all(),
+            'posts' => Post::orderBy('id', 'desc')->take('20')->get(),
         ]);
     }
 
