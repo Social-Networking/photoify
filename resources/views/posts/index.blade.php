0bb7927658dc5ff1ef43bcfbaaac357daf3fd4d3
laykith <laykith@gmail.com>
2019-01-03 04:48:25 +0100

    Moved post card layout into its own view

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,36 +3,7 @@
 @section('content')
 <div class="card-container">
     @foreach($posts as $post)
-
-    <div class="card has-background-dark">
-        <div class="card-body is-transparent" style="padding: 0px;">
-            <div class="card-image">
-                <figure class="image is-square">
-                    <img src="{{ asset('images/'.$post->image) }}">
-                </figure>
-            </div>
-
-            <div class="card-userinfo">
-                <div class="is-flex is-v-center">
-                    <figure class="image is-48x48">
-                        <img class="is-rounded" src="{{ asset('images/'.$post->image) }}">
-                    </figure>
-                    <div class="user-text">
-                        <p class="is-size-3 has-text-white">{{ $post->user->name }}</p>
-                        <a class="is-size-6 has-text-info" href="{{  route('profile', ['id' => $post->user->id]) }}">{{ '@'.$post->user->name }}</a>
-                    </div>
-                </div>
-
-                <a class="like"><i class="fas fa-heart"></i></a>
-            </div>
-
-            <div class="card-content">
-                <p class="lead is-size-4 has-text-white">{{ $post->description }}</p>
-                <div class="card-timebox"><time class="has-text-grey-light" datetime="{{ $post->created_at }}"></time></div>
-            </div>
-        </div>
-    </div>
+        @include('layouts.post', $post)
     @endforeach
-
 </div>
 @endsection
--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -8,7 +8,7 @@
                 </figure>
                 <div>
                     <h1 class="title">
-                        {{ $user->name }}
+                        {{ $user->display_name !== NULL ? $user->display_name : $user->name }}
                     </h1>
                     <h2 class="subtitle">
                         {{ '@'.$user->name }}
@@ -22,36 +22,7 @@
 
 <div class="card-container">
     @foreach($user->posts as $post)
-
-    <div class="card has-background-dark">
-        <div class="card-body is-transparent" style="padding: 0px;">
-            <div class="card-image">
-                <figure class="image is-square">
-                    <img src="{{ asset('images/'.$post->image) }}">
-                </figure>
-            </div>
-
-            <div class="card-userinfo">
-                <div class="is-flex">
-                    <figure class="image is-48x48">
-                        <img class="is-rounded" src="{{ asset('images/'.$post->image) }}">
-                    </figure>
-                    <div>
-                        <p class="title is-4 has-text-white">{{ $post->user->name }}</p>
-                        <p class="subtitle is-6 has-text-info">{{ '@'.$post->user->name }}</p>
-                    </div>
-                </div>
-
-                <a class="like"><i class="fas fa-heart"></i></a>
-            </div>
-
-            <div class="card-content">
-                <p class="lead is-size-4 has-text-white">{{ $post->description }}</p>
-                <div class="card-timebox"><time class="has-text-grey-light" datetime="{{ $post->created_at }}"></time></div>
-            </div>
-        </div>
-    </div>
+        @include('layouts.post', $post)
     @endforeach
-
 </div>
 @endsection
