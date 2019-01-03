0402963f5ab98fd0aef563e80f4a671cee8e5d52
laykith <laykith@gmail.com>
2019-01-03 09:30:47 +0100

    Added default user avatar

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('image')->nullable();
+            $table->string('image')->default('default.png');
             $table->string('name');
             $table->string('display_name')->nullable();
             $table->string('email')->unique();
