dbf3adff6e6b5c93616fb3699f09b3ca3bf6f9f8
laykith <laykith@gmail.com>
2019-01-03 09:06:45 +0100

    Added account settings updating

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
@@ -21,11 +22,31 @@ class UsersController extends Controller
         ]);
     }
 
-    public function update()
+    public function update(Request $request)
     {
+        $user = User::find(Auth::id());
+
         $request->validate([
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'description' => 'required|max:255',
-        ]);
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'display_name' => 'string|max:32',
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:6',
+            //'new_password' => 'string|min:6|different:password',
+            ]);
+
+        //Update image if new one provided
+        if (null !== $request->image) {
+            $user->image = $request->image;
+            $imageName = time().'.'.$request->image->getClientOriginalExtension();
+            $request->image->move(public_path('images'), $imageName);
+        }
+
+        //Update rest if set.
+        strlen($request->display_name) > 0 ? $user->display_name = $request->display_name : '';
+        strlen($request->new_password) > 0 ? $user->password = Hash::make($request->new_password) : '';
+
+        $user->save();
+
+        return redirect('user/'.Auth::id());
     }
 }
--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'display_name', 'image', 'name', 'email', 'password',
     ];
 
     /**
