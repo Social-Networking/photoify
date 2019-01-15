de547664bc42edfac73d9ffad8a301f2c7875f5b
laykith <laykith@gmail.com>
2019-01-15 08:26:33 +0100

    Updated landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 <div class="index-container">
-    <div class="index-header is-flex is-justify-center w100">
-        {!! file_get_contents(public_path('svg/photoify_logo.svg')) !!}
-    </div>
+
     <div class="message has-background-dark">
-        <h1 class="is-size-2 has-text-white has-text-centered mb">{{ __('Welcome to Photoify!') }}</h1>
+        <div class="index-header is-flex is-justify-center w100">
+            {!! file_get_contents(public_path('svg/photoify_logo.svg')) !!}
+        </div>
         <p class="is-size-4 has-text-white has-text-centered mb">{{ __('Create an account today and experience the
             future of social media') }}</p>
         <div class="buttons has-addons">
