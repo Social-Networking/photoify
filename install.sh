da615d92a91fa4d4677c995d3090ce763890acbe
André Broman <laykith@gmail.com>
2019-01-15 13:45:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ composer install;
 
 #Make .env file and add absolute database path
 mv .env.example .env;
-echo "DB_DATABASE="\$(pwd)/database/photoify.db\"" >> .env;
+echo "DB_DATABASE=\"$(pwd)/database/photoify.db\"" >> .env;
 
 #Generate app key
 php artisan key:generate;
