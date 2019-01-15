0aaef5f10ae8a2e75e1a815ca048fc9e0affeb28
laykith <laykith@gmail.com>
2019-01-15 13:39:02 +0100

    Fixed change password

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,7 @@ use App\User;
 use App\Follow;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UsersController extends Controller
 {
